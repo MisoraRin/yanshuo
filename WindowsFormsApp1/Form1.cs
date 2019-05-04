b23ebffd037b36d5590f1329e3f39698f983731a
su cheng <qj13769709078@live.com>
2019-05-04 14:26:39 +0800

    添加了图片生成代码

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -8,6 +8,17 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Drawing.Imaging;
+using System.IO;
+
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
@@ -15,6 +26,71 @@ namespace WindowsFormsApp1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        static int x = 0, y = 0, h = 1368, w = 768;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Bitmap bitmap = new Bitmap(1920, 1080);
+            Graphics g = Graphics.FromImage(bitmap);
+            SolidBrush mybrush = new SolidBrush(Color.FromArgb(230, 230, 230));
+            g.FillRectangle(mybrush,new Rectangle(0,0,1920,1080));
+            // g.Clear(Color.Transparent);
+           
+           // g.Dispose();
+            Font myfont;
+            myfont = new Font("方正细金陵简体", 50);         //设置默认字体格式
+
+            StringFormat sf = new StringFormat();
+            sf.Alignment = StringAlignment.Center;
+            sf.LineAlignment = StringAlignment.Center;
+            SolidBrush newbrush = new SolidBrush(Color.FromArgb(1, 1, 1));
+            g.DrawString("哈哈哈哈哈哈", myfont, newbrush, new Rectangle(0, 0, 1920,1080), sf);
+
+            bitmap.Save("C:\\Users\\zwt\\Desktop\\h.png", ImageFormat.Png);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Image a= Image.FromFile("C:\\Users\\zwt\\Desktop\\timg.jpg");
+            
+            Graphics g = Graphics.FromImage(a);
+            SolidBrush mybrush;
+            mybrush = new SolidBrush(Color.Lime);  //设置默认画刷颜色
+            Font myfont;
+            myfont = new Font("方正细金陵简体", 14);         //设置默认字体格式
+            g.DrawString("厉害了word歌", myfont, mybrush, new Rectangle(0, 0, 500, 500));
+            StringFormat sf=new StringFormat();
+            sf.Alignment = StringAlignment.Center;
+            sf.LineAlignment = StringAlignment.Center;
+
+            g.DrawString("哈哈哈哈哈哈", myfont, mybrush, new Rectangle(0, 0, a.Height, a.Width), sf);
+            
+            
+            a.Save("C:\\Users\\zwt\\Desktop\\timg1.jpg");
+        }
+        public static string GetHttpResponse(string url, int Timeout)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            request.ContentType = "text/html;charset=UTF-8";
+            request.UserAgent = null;
+            request.Timeout = Timeout;
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream myResponseStream = response.GetResponseStream();
+            StreamReader myStreamReader = new StreamReader(myResponseStream, Encoding.GetEncoding("utf-8"));
+            string retString = myStreamReader.ReadToEnd();
+            myStreamReader.Close();
+            myResponseStream.Close();
+
+            return retString;
         }
     }
 }
